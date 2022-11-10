27794037c426bb3cfe14e649aab23cbd7cb1a82b
Luiz Galvão <85366930+luizloro@users.noreply.github.com>
2022-11-10 10:29:57 -0300

    Variável em Scopo Local

--- variaveis.js
+++ variaveis.js
@@ -14,13 +14,29 @@
 // name = "Carlos"
 // console.log(name)
 
-//Globais
+//Variáveis Globais (Scope Global)
 //Hoisting -> SOMENTE O VAR REALIZA ESSE METODO(Pega uma variável dentro de um bloco e tranforma ela em variável global)
-console.log('Variável name existe aqui ?', name)
+// console.log('Variável name existe aqui ?', name)
+
+// {//Scopo (Bloco de código)
+//     //Local
+//     var name = 'Luan'
+// }
+
+// console.log('Variável name existe aqui ?', name)
+
+
+//Variáveis Locais (Scope Local)
+let name = "luan"
+
+console.log('Meu nome chegou aqui ? ?', name)
+
 
 {//Scopo (Bloco de código)
     //Local
-    var name = 'Luan'
+    //Veja que no resultado do console a variável "name" agora tem o valor de "Jenifer", porque dentro do Scopo a Variável respeita o que foi atribuido a ela dentro do BLOCO DE CÓDIGO DO SCOPO LOCAL
+    let name = "Jenifer"
+    console.log('Meu nome chegou aqui ? ?', name)
 }
 
-console.log('Variável name existe aqui ?', name)
+console.log('Meu nome chegou aqui ? ?', name)
