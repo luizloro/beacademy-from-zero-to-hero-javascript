0970f69a60024e9ef4dea1a5339b1f04701ac4ee
Luiz Galvão <85366930+luizloro@users.noreply.github.com>
2022-11-10 11:12:05 -0300

    Arrays em JavaScript

---
+++ arrays.js
@@ -0,0 +1,16 @@
+const animals = [
+    "Elefante",//0
+    "  Gato",//1
+    "Leão",//2
+    "Caachorro",//3
+    {//.name
+        name: "Moaciar",
+        age: 2,
+        weight:4
+    }
+]
+//Tamanho 5 
+
+
+
+console.log(animals[4].name)
