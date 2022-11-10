fd9e69775ae397839690786f9abd5739cfee6aea
Luiz Galvão <85366930+luizloro@users.noreply.github.com>
2022-11-10 09:44:51 -0300

    Declaração de variaveis

---
+++ variaveis.js
@@ -0,0 +1,15 @@
+//Declarar 
+
+// var name
+// let name2
+// const name3 = "Meu Nome"
+
+// As variáveis n JavaScript são fracamente tipadas e dinamica ex:
+// let luan = 'Luan'
+// console.log(name)
+
+// name = 32
+// console.log(name)
+
+// name = "Carlos"
+// console.log(name)
