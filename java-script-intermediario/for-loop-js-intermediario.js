acddfac4389315b29c21153f92c2843f62f54a8d
Luiz Galvão <85366930+luizloro@users.noreply.github.com>
2022-11-24 10:39:09 -0300

    For/Loop com JavaScript intermediário

---
+++ java-script-intermediario/for-loop-js-intermediario.js
@@ -0,0 +1,25 @@
+//For / Loop 
+//Utilizado para fazer algo repetidamente até que uma condição seja atingida.
+
+for(let numero = 0; numero <=10; numero ++) {
+    
+    if(numero % 2 == 0) {
+        console.log(`Número ${numero} é PAR`)
+
+    }else {
+        console.log(`Número ${numero} é IMPAR`)
+    }
+
+}
+//Retorna de 0 a 10 no console
+
+const videoGames = [
+    'Switch',
+    'PS4',
+    'XBOX'
+]
+
+for(let i = 0; i < videoGames.length; i++) {
+    console.log(`Consoles nº ${i + 1} é ${videoGames[i]}`)
+
+}
